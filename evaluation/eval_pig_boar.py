a9b2a1b28bbf493ba3140f0016c96d85c0f2bf7f
tommccoy1 <tom.mccoy@jhu.edu>
2024-08-19 00:19:17 -0400

    Update eval_pig_boar.py

--- evaluation/eval_pig_boar.py
+++ evaluation/eval_pig_boar.py
@@ -91,7 +91,7 @@ def pig_encode(sentence, suffix="ay", vowel_consonant=""):
     return " ".join(new_sentence)
 
 
-for model in ["gpt-3.5-turbo-0613", "gpt-4-0613", "llama-2-70b-chat", "text-bison-001", "llama-3-70b-chat-hf", "claude-3-opus-20240229", "olmo-7b-instruct", "gemini-1.0-pro-001"]:
+for model in ["gpt-3.5-turbo-0613", "gpt-4-0613", "llama-3-70b-chat-hf", "claude-3-opus-20240229", "gemini-1.0-pro-001"]:
     print("")
     print(model)
     for suffix in ["ay", "uv"]:
